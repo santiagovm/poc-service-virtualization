c26c650c2c21306678257e5d073b395bd6265ce8
Santiago Vasquez <svasquez@pivotal.io>
2020-06-28 14:27:55 -0400

    created empty project

---
+++ src/main/java/com/vasquezhouse/pocservicevirtualization/ServiceVirtualizationPoCApplication.java
@@ -0,0 +1,13 @@
+package com.vasquezhouse.pocservicevirtualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceVirtualizationPoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceVirtualizationPoCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vasquezhouse/pocservicevirtualization/ServiceVirtualizationPoCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasquezhouse.pocservicevirtualization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceVirtualizationPoCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
