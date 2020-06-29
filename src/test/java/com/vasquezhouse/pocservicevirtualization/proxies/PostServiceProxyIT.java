034fc9a1e75df0bf7725dcdd3ecf577f45c1c706
Santiago Vasquez <svasquez@pivotal.io>
2020-06-28 22:12:00 -0400

    configured wiremock to record and playback

--- src/test/java/com/vasquezhouse/pocservicevirtualization/proxies/PostServiceProxyIT.java
+++ src/test/java/com/vasquezhouse/pocservicevirtualization/proxies/PostServiceProxyIT.java
@@ -1,16 +1,19 @@
 package com.vasquezhouse.pocservicevirtualization.proxies;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.recording.RecordSpec;
+import com.github.tomakehurst.wiremock.recording.RecordingStatus;
 import com.vasquezhouse.pocservicevirtualization.ServiceVirtualizationPoCApplication;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.recordSpec;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -18,39 +21,77 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         classes = ServiceVirtualizationPoCApplication.class
 )
-@AutoConfigureWireMock(port = 0) // 0 = use random port
 public class PostServiceProxyIT {
+
+    @Value("${postServiceBaseUrl}")
+    private String postServiceBaseUrl;
+
+    @Value("${wiremock.useMockResponses:true}")
+    private boolean useMockResponses;
+
+    @Value("${wiremock.makeStubsPersistent:false}")
+    private boolean persistRecordings;
+
+    @Value("${wiremock.maxTextBodySize:0}")
+    private int maxTextBodySize;
 
     private PostServiceProxy postService;
 
-    @Autowired
-    Environment env;
+    WireMockServer wireMockServer;
 
     @BeforeEach
     public void setup() {
 
-        String wiremockPort = env.getProperty("wiremock.server.port");
-        String baseUrl = "http://localhost:" + wiremockPort;
+        RecordSpec theRecordSpec = recordSpec()
+                .forTarget(postServiceBaseUrl)
+                .makeStubsPersistent(persistRecordings)
+                .extractTextBodiesOver(maxTextBodySize)
+                .captureHeader("Content-Type")
+                .build();
+
+        final int WIREMOCK_RANDOM_PORT = 0;
+        wireMockServer = new WireMockServer(WIREMOCK_RANDOM_PORT);
+        wireMockServer.start();
+
+        int wireMockPort = wireMockServer.port();
+        String wireMockBaseUrl = "http://localhost:" + wireMockPort;
+
+        if (!useMockResponses) {
+            wireMockServer.startRecording(theRecordSpec);
+        }
 
         WebClient webClient = WebClient
                 .builder()
                 .clone()
-                .baseUrl(baseUrl)
+                .baseUrl(wireMockBaseUrl)
                 .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
                 .build();
 
         postService = new PostServiceProxy(webClient);
     }
 
+    @AfterEach
+    public void tearDown() {
+
+        if (wireMockServer.getRecordingStatus().getStatus().equals(RecordingStatus.Recording)) {
+            wireMockServer.stopRecording();
+        }
+
+        wireMockServer.stop();
+    }
+
     @Test
     public void verifyExistingPost() {
 
         // act
-        PostInfo post = postService.findByPostId("333").block();
+        String expectedPostId = "1";
+        PostInfo post = postService.findByPostId(expectedPostId).block();
 
         // assert
-        assertEquals("333", post.getId());
-        assertEquals("foo title", post.getTitle());
+        String expectedTitle = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit";
+
+        assertEquals(expectedPostId, post.getId());
+        assertEquals(expectedTitle, post.getTitle());
     }
 
     @Test
